93449eb6b579f060045358094391e65a3927f667
garylee0122 <dagigilee0122@gmail.com>
2026-05-22 16:59:46 +0800

    To enable nginx to proxy both Laravel and ASP.NET

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,61 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+use Illuminate\Http\Request;
+use App\Jobs\SendOrderJob;
+
+/*
+|--------------------------------------------------------------------------
+| Laravel Web 首頁
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Laravel API
+| 為了滿足 nginx 的 location 配置，我們將 API 路由放在 web.php 中，並使用前綴 'laravel-api' 來區分。
+|--------------------------------------------------------------------------
+*/
+
+Route::prefix('laravel-api')->group(function () {
+
+    Route::get('/test', function () {
+        return response()->json([
+            'message' => 'Laravel API OK'
+        ]);
+    });
+
+    Route::post('/register', [AuthController::class, 'register']);
+
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+
+        Route::apiResource('products', ProductController::class);
+
+        Route::post('orders', [OrderController::class, 'store']);
+
+        Route::get('orders', [OrderController::class, 'index']);
+
+        Route::get('orders/{id}', [OrderController::class, 'show']);
+    });
+
+    Route::get('/test-queue', function () {
+
+        SendOrderJob::dispatch(123);
+
+        return "Job dispatched!";
+    });
+
 });
